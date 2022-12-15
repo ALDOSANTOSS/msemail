ea2a49977cd02bb8d48089eb57e1e23434e7045a
adao <aldo.santana@gft.com>
2022-12-15 19:55:19 -0300

    Initial commit

---
+++ src/main/java/com/ms/email/dto/EmailDto.java
@@ -0,0 +1,22 @@
+package com.ms.email.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record EmailDto(
+        @NotBlank
+        String ownerRef,
+        @NotBlank
+        @Email
+        String emailFrom,
+        @NotBlank
+        @Email
+        String emailTo,
+        @NotBlank
+        String subject,
+        @NotBlank
+        String text
+
+) {
+}
